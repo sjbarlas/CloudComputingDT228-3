dc39848516cadc0acbfbd9dc293a12f5e27391f5
sjbarlas <c12809080@mydit.ie>
2015-09-30 15:03:32 +0000

    input number

--- Lab3/Fibonacci.py
+++ Lab3/Fibonacci.py
@@ -1,5 +1,6 @@
 # Fibonacci for 1000
 
+# getting the recursive sequence
 def fib(n):
   if n == 0 or n == 1
   return n
@@ -7,3 +8,6 @@ def fib(n):
 else:
   return fib(n-1) + fib(n-2)
 
+# asking the user to input the number
+input = int(raw_input("Please enter your number: "))
+output = fib(input)
