2880709a9505ea1a354ae5ea7caf8f42608a8ee5
sjbarlas <c12809080@mydit.ie>
2015-09-30 14:50:33 +0000

    final commit

--- Lab3/Palindrome.py
+++ Lab3/Palindrome.py
@@ -5,3 +5,11 @@ word = raw_input("Please enter your word: ")
 
 # reverse the input word
 rev_word = reversed(word)
+
+# if the word and the word reversed is the same then print they are palindromes
+if list(word) == list(rev_word):
+  print "This is a palindrome!"
+
+# if the words are not palindromes then print this
+else:
+  print "This is not a palindrome!"
