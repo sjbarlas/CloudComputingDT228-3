2bf496bf8ce2379d8129283c5327093b1488dc91
sjbarlas <c12809080@mydit.ie>
2015-09-30 15:48:59 +0000

    sunday start

--- Lab3/Sunday.py
+++ Lab3/Sunday.py
@@ -1,1 +1,7 @@
 # Sundays
+
+# 1 Jan 1900 = Monday
+# 30 days has Sept, April, June, Nov
+# 28 Feb [ leap years = 29]
+# A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+# How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
