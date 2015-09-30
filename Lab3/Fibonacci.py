d4fc049d978f77ee6a2b9cd1d576f9bc9ec66703
sjbarlas <c12809080@mydit.ie>
2015-09-30 15:14:11 +0000

    printing the sequence

--- Lab3/Fibonacci.py
+++ Lab3/Fibonacci.py
@@ -19,3 +19,15 @@ print "This is the corresponding fibonacci number: %d" % output
 # displaying the length of the fibonacci number
 length = len(str(output))
 print "This is the length of your fibonacci number: %d" % length
+
+# using for loop to print the fibonacci sequence up to the input number
+for x in range (0, fibnumber):
+  print (fib(x))
+
+  # if the sequence has a number where it is made up of 4 numbers, print this
+  if length >= 4:
+    print "Your fibonacci is composed of 4 numbers!"
+
+  # print this if the above is not valid
+  else:
+    print "Your number is not made up of 4 numbers!"
